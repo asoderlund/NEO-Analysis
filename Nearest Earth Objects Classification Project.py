cd773a2e8b7451b723db40ca4041dc704399c35d
asoderlund <70977315+asoderlund@users.noreply.github.com>
2022-07-20 14:45:12 -1000

    Add files via upload

--- Nearest Earth Objects Classification Project.py
+++ Nearest Earth Objects Classification Project.py
@@ -1,150 +1,72 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# # Nearest Earth Objects Portfolio Project
-
-# In[1]:
-
+# # Nearest Earth Objects Portfolio Project: Alyssa Soderlund
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import precision_recall_fscore_support as score
 from sklearn.metrics import roc_curve, auc
 
 
-# In[2]:
-
-
 df = pd.read_csv('neo_v2.csv')
-
-
-# In[3]:
-
 
 # how many records?
 len(df)
 
-
-# In[4]:
-
-
 # how many attributes?
 df.columns
-
-
-# In[5]:
-
 
 # peek at the data
 df.head(10)
 
-
-# ### Attributes
-# - id: identifier (same ones are used multiple times)
-# - name: given by NASA
-# - est_diameter_min: minimum estimated diameter in kilometers
-# - est_diameter_max: maximum estimated diameter in kilometers
-# - relative_velocity: velocity relative to earth
-# - miss_distance: distance in kilometers missed
-# - orbiting_body: planet that the asteroid orbits
-# - sentry_object: Included in sentry - an automated collision monitoring system
-# - absolute_magnitude: intrinsic luminosity
-# - hazardous: whether the asteriod is harmful or not
-
-# In[6]:
-
-
+# How many unique values for each attribute?
 print(df.nunique())
 
 
 # Delete orbiting_body and sentry_object
-
-# In[7]:
-
-
 df.drop(["orbiting_body", "sentry_object"], axis = 1, inplace = True)
 
-
-# In[8]:
-
-
-df.head()
-
-
-# See why same id is recorded several times
-
-# In[9]:
-
-
+# Look at just one object- what changes for each observation?
 df[df['id'] == 2512244]
 
-
-# See if categories are disproportionate  
-# about 10% of asteroids are hazardous
-
-# In[10]:
-
-
+# Is there a class imbalance for hazardous?
 print(df["hazardous"].value_counts() / len(df))
 
-
-# In[11]:
-
-
-sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+sns.countplot(x='hazardous',data=df)
+plt.suptitle('Number of Non-Hazardous and Hazardous Objects')
+plt.rcParams['figure.figsize']=[4,4]
 plt.show()
 
+# Look at bivariate correlations
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+plt.suptitle('Bivariate Correlations')
+plt.show()
 
-# Delete the diameter vars because they are perfectly negatively correlated with absolute magnitude
-
-# In[12]:
-
-
+# Drop one diameter variable since they are perfectly correlated
 df.drop(["est_diameter_min"], axis = 1, inplace = True)
 
-
-# In[13]:
-
-
 # Extracting year from name to see later on whether we could have a pattern
-df[['drop','work']]=df.name.str.split('(',expand=True)
-
-
-# In[14]:
-
+df[['drop','temp']]=df.name.str.split('(',expand=True)
 
 df.drop(columns='drop',inplace=True)
 
-def year_extract(x):
+def get_year(x):
     return x.strip()[0:x.strip().index(' ')]
-df['year']=df['work'].apply(year_extract)
+df['year']=df['temp'].apply(get_year)
+df.drop(columns='temp', inplace=True)
 
-
-# In[15]:
-
-
-df.drop(columns='work', inplace=True)
-
-
-# In[16]:
-
-
+# Check year labels
 df['year'].unique()
 
-
-# In[17]:
-
-
-# [A911,6743,A898,4788,6344,A924,A/2019] --> Years to be changed 
+# Change abnormal years to the correct year
 
 df.loc[df.year=='A911','year']='1911' 
 df.loc[df.year=='6743','year']='1960'
@@ -156,106 +78,53 @@ df.loc[df.year=='4788','year']='1960'
 
 df.year.unique()
 
+# Change years to ints
+df.year=df.year.astype(int)
 
-# In[42]:
+# Boxplot for year and target variable
+sns.boxplot(x='hazardous',y='year',data=df)
+plt.suptitle('Box Plots for Hazardous Objects by Year')
+plt.show()
 
 
-#sns.boxplot(x='hazardous',y='year',data=df)
-#plt.show()
 
-
-# Inference from the boxplot
-# 
-#     Hazardous objects were mostly discovered between around 2002 to before 2020
-#     There were many non-hazardous objects discovered pre-1980s (is this because hazardous objects tend to be farther away so harder to detect with older equipment, also non-hazardous objects tend to have higher magnitude so easier to spot)
 
 # ## Univariate and Bivariate analysis
+num_cols = ["est_diameter_max", "relative_velocity", "miss_distance", "absolute_magnitude","year"]
 
-# In[20]:
-
-
-num_cols = ["est_diameter_max", "relative_velocity", "miss_distance", "absolute_magnitude"]
-
-
-# In[21]:
-
-
+# Univariate distributions for numerical variables
 rows=2
-cols=2
+cols=3
 count=1
-plt.rcParams['figure.figsize']=[10,10]
+plt.rcParams['figure.figsize']=[15,9]
 for i in num_cols:
     plt.subplot(rows,cols,count)
     sns.distplot(df[i], color='c')
     count+=1
-plt.tight_layout()
+plt.suptitle('Distributions of Numerical Variables')
 plt.show()
 
-
-# Inferences From the subplots
-# 
-#     Estimated minimum Diameter and estimated maximum Diameter are highly positively skewed with sharp spike at on end, indicating the presence of outliers
-#     Relative Velocity is mildly positive skewed
-#     miss_distance is uniform over the data
-#     Absolute magnitude is the closest one to a normal distribution curve
-
-# In[23]:
+# Pairs plots, color coded by hazardous classification
+sns.pairplot(df[num_cols+['hazardous']],hue = 'hazardous')
 
 
-sns.pairplot(df[num_cols+['hazardous','year']],hue = 'hazardous')
 
-
-# In[24]:
-
-
-# Making subplots for multiple boxplots and comparing with our target variable
-
-rows=3
-cols=2
-counter=1
-plt.rcParams['figure.figsize']=[10,8]
-for i in num_cols:
-    plt.subplot(rows,cols,counter)
-    sns.boxplot(x='hazardous',y=i,data=df)
-    counter+=1
-    
-plt.tight_layout()
-plt.show()
-
-
-# Inferences from the boxplots:
-# 
-#     Non-Hazardous objects have a higher estimated minimum and maximum diameter
-#     Our Hypothesis is that Hazardous objects have a higher relative_velocity
-#     It seems that hazardous objects have a slightly higher miss_distance
-#     The graphs indicate that Non-Hazardous objects have considerably higher absolute_magnitude.
 
 # # Classifier Models
 
-# In[25]:
-
-
+# Create dataframes for predictor and target variables
 X = df.drop(["id","name","hazardous"], axis=1)
 y = df.hazardous.astype(int)
 
-
-# In[26]:
-
-
+# Test train split 80/20, stratify y due to class imbalance
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0, stratify=y)
 
+# Scale x for K nearest neighbors
+sc=StandardScaler()
+X_train_scaled=pd.DataFrame(sc.fit_transform(X_train))
+X_test_scaled=pd.DataFrame(sc.transform(X_test))
 
-# In[27]:
-
-
-sc=StandardScaler() # Machine Instance
-X_train_scaled=pd.DataFrame(sc.fit_transform(X_train)) # Scaling the train set
-X_test_scaled=pd.DataFrame(sc.transform(X_test)) # Scaling the test set
-
-
-# In[28]:
-
-
+# Define functions for plotting ROC curves
 def roc_curve_plot(y_test, y_scores, method):
     fpr, tpr, threshold = roc_curve(y_test, y_scores[:, 1])
     roc_auc = auc(fpr, tpr)
@@ -265,20 +134,19 @@ def roc_curve_plot(y_test, y_scores, method):
     plt.ylabel('True Positive Rate')
     plt.xlabel('False Positive Rate')
     plt.title('ROC Curve of ' + method)
+    plt.rcParams['figure.figsize']=[6,5]
     plt.show()
     return roc_auc
 
 
 # ## Decision Tree
 
-# In[29]:
-
-
-# DecisionTree Model
-
+# Create decision tree model
 DT = DecisionTreeClassifier()
-DT.fit(X_train_scaled, y_train)
+tree = DT.fit(X_train_scaled, y_train)
 DT_pred = DT.predict(X_test_scaled)
+
+# Create metrics table
 Acc_DT = round(accuracy_score(DT_pred, y_test), 4)
 xgprec_DT, xgrec_DT, xgf_DT, support_DT = score(y_test, DT_pred)
 precision_DT, recall_DT, f1_DT = round(xgprec_DT[0], 4), round(xgrec_DT[0],4), round(xgf_DT[0],4)
@@ -287,22 +155,36 @@ scores_DT = pd.DataFrame({
     'Score': [Acc_DT, precision_DT, recall_DT, f1_DT]})
 scores_DT
 
-
-# In[30]:
-
-
+# Plot ROC curve
 y_scores_DT = DT.predict_proba(X_test_scaled)
 auc_DT = roc_curve_plot(y_test, y_scores_DT, 'Decision Tree')
+
+# Variable importance plot
+feat_importances = pd.Series(DT.feature_importances_, index=X.columns)
+feat_importances.plot(kind='barh', title='Variable Importance for Decision Tree',figsize=[5,3])
 
 
 # ## K Nearest Neighbors
 
-# In[31]:
+# Hyperparameter tuning for k
+error_rates = []
+for i in np.arange(1, 40):
+    model = KNeighborsClassifier(n_neighbors = i)
+    model.fit(X_train_scaled, y_train)
+    predictions = model.predict(X_test_scaled)
+    error_rates.append(np.mean(predictions != y_test))
+
+plt.rcParams['figure.figsize']=[6,4]
+plt.suptitle('Error Rates for k from 1 to 40')
+plt.plot(error_rates)
 
 
-KNN = KNeighborsClassifier(n_neighbors = 4)
+# Create KNN model
+KNN = KNeighborsClassifier(n_neighbors = 15)
 KNN.fit(X_train_scaled, y_train)
 KNN_pred = KNN.predict(X_test_scaled)
+
+# Create metrics table
 Acc_KNN = round(accuracy_score(KNN_pred, y_test), 4)
 xgprec_KNN, xgrec_KNN, xgf_KNN, support_KNN = score(y_test, KNN_pred)
 precision_KNN, recall_KNN, f1_KNN = round(xgprec_KNN[0], 4), round(xgrec_KNN[0],4), round(xgf_KNN[0],4)
@@ -312,22 +194,31 @@ scores_KNN = pd.DataFrame({
 scores_KNN
 
 
-# In[32]:
-
-
+# Plot ROC curve
 y_scores_KNN = KNN.predict_proba(X_test_scaled)
 auc_KNN = roc_curve_plot(y_test, y_scores_KNN, 'kNN')
 
 
 # ## Random Forest
 
-# In[33]:
+# Hyperparameter tuning
+max_depth=[2, 8, 16]
+n_estimators = [64, 128, 256]
+param_grid = dict(max_depth=max_depth, n_estimators=n_estimators)
+
+dfrst = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+grid = GridSearchCV(estimator=dfrst, param_grid=param_grid, cv = 5)
+grid_results = grid.fit(X_train_scaled, y_train)
+
+print("Best: {0}, using {1}".format(grid_results.cv_results_['mean_test_score'], grid_results.best_params_))
 
 
-# Random Forest Model
+# Create model
 RF = RandomForestClassifier()
 RF.fit(X_train_scaled, y_train)
 RF_pred = RF.predict(X_test_scaled)
+
+# Create metrics table
 Acc_RF = round(accuracy_score(RF_pred, y_test), 4)
 xgprec_RF, xgrec_RF, xgf_RF, support_RF = score(y_test, RF_pred)
 precision_RF, recall_RF, f1_RF = round(xgprec_RF[0], 4), round(xgrec_RF[0],4), round(xgf_RF[0],4)
@@ -336,23 +227,23 @@ scores_RF = pd.DataFrame({
     'Score': [Acc_RF, precision_RF, recall_RF, f1_RF]})
 scores_RF
 
-
-# In[34]:
-
-
+# Plot ROC curve
 y_scores_RF = RF.predict_proba(X_test_scaled)
 auc_RF = roc_curve_plot(y_test, y_scores_RF, 'Random Forest')
+
+# Variable importance plot
+feat_importances_RF = pd.Series(RF.feature_importances_, index=X.columns)
+feat_importances_RF.nlargest(8).plot(kind='barh', title = 'Variable Importance for Random Forest', figsize=[5,3])
 
 
 # ## Gradient Boosted Decision Tree
 
-# In[35]:
-
-
-# Gradient Boosted Decision Tree Model
+# Create model
 XGB = XGBClassifier()
 XGB.fit(X_train_scaled, y_train)
 XGB_pred = XGB.predict(X_test_scaled)
+
+# Create metrics table
 Acc_XGB = round(accuracy_score(XGB_pred, y_test),4)
 xgprec_XGB, xgrec_XGB, xgf_XGB, support_XGB = score(y_test, XGB_pred)
 precision_XGB, recall_XGB, f1_XGB = round(xgprec_XGB[0], 4), round(xgrec_XGB[0],4), round(xgf_XGB[0],4)
@@ -361,68 +252,23 @@ scores_XGB = pd.DataFrame({
     'Score': [Acc_XGB, precision_XGB, recall_XGB, f1_XGB]})
 scores_XGB
 
-
-# In[36]:
-
-
+# Plot ROC Cruve
 y_scores_XGB = XGB.predict_proba(X_test_scaled)
 auc_XGB = roc_curve_plot(y_test, y_scores_XGB, 'Gradient Boosted Decision Tree')
 
+# Variable importance plot
+feat_importances_XGB = pd.Series(XGB.feature_importances_, index=X.columns)
+feat_importances_XGB.plot(kind='barh', title = 'Variable Importance for Gradient Boosted Tree', figsize=[5,3])
 
-# # Compare Models
 
-# In[37]:
+# # Compare Models table
 
-
-accuracy_table = pd.DataFrame({
+total_table = pd.DataFrame({
     'Model': ['Random Forest', 'XG Boost','K Nearest Neighbors', 'Decision tree'],
-    'Accuracy': [Acc_RF, Acc_XGB, Acc_KNN, Acc_DT]})
-accuracy_table.sort_values(by='Accuracy', ascending=False)
-
-
-# In[38]:
-
-
-precision_table = pd.DataFrame({
-    'Model': ['Random Forest', 'XG Boost','K Nearest Neighbors', 'Decision tree'],
-    'Precision': [precision_RF, precision_XGB, precision_KNN, precision_DT]})
-precision_table.sort_values(by='Precision', ascending=False)
-
-
-# In[39]:
-
-
-recall_table = pd.DataFrame({
-    'Model': ['Random Forest', 'XG Boost','K Nearest Neighbors', 'Decision tree'],
-    'Recall': [recall_RF, recall_XGB, recall_KNN, recall_DT]})
-recall_table.sort_values(by='Recall', ascending=False)
-
-
-# In[40]:
-
-
-f1_table = pd.DataFrame({
-    'Model': ['Random Forest', 'XG Boost','K Nearest Neighbors', 'Decision tree'],
+    'AUC': [auc_RF, auc_XGB, auc_KNN, auc_DT],
+    'Accuracy': [Acc_RF, Acc_XGB, Acc_KNN, Acc_DT],
+    'Precision': [precision_RF, precision_XGB, precision_KNN, precision_DT],
+    'Recall': [recall_RF, recall_XGB, recall_KNN, recall_DT],
     'F1': [f1_RF, f1_XGB, f1_KNN, f1_DT]})
-f1_table.sort_values(by='F1', ascending=False)
-
-
-# In[41]:
-
-
-auc_table = pd.DataFrame({
-    'Model': ['Random Forest', 'XG Boost','K Nearest Neighbors', 'Decision tree'],
-    'AUC': [auc_RF, auc_XGB, auc_KNN, auc_DT]})
-auc_table.sort_values(by='AUC', ascending=False)
-
-
-# Overall, it is clear that the random forest model performed the best  
-# Random forest was not the best in recall and precision  
-# Gradient boosted decision tree was a close second  
-# If recall is most important, may be better to use XGB
-
-# In[ ]:
-
-
-
+total_table.sort_values(by='AUC', ascending=False)
 
